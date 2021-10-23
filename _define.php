8a46fad36e2bc6726340d4f884a538c1f845ec50
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:07 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,20 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Gravatar",                                                          // Name
-    "Add Gravatar/Libravatar images to your posts and comments authors", // Description
-    "Franck Paul",                                                       // Author
+    'Gravatar',                                                          // Name
+    'Add Gravatar/Libravatar images to your posts and comments authors', // Description
+    'Franck Paul',                                                       // Author
     '0.9',                                                               // Version
     [
         'requires'    => [['core', '2.13']], // Dependencies
         'permissions' => 'admin',            // Permissions
         'type'        => 'plugin',           // Type
         'settings'    => [],                 // Settings
-        'support'     => 'https://github.com/franck-paul/gravatar',
-        'details'     => 'https://open-time.net/?q=gravatar'
+
+        'details'    => 'https://open-time.net/?q=gravatar',       // Details URL
+        'support'    => 'https://github.com/franck-paul/gravatar', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/gravatar/main/dcstore.xml'
     ]
 );
