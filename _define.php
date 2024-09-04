ad73fca1d53ecacb6ae7d775c05221db440c0660
franck <carnet.franck.paul@gmail.com>
2024-09-04 10:11:47 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Gravatar',
     'Add Gravatar/Libravatar images to your posts and comments authors',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
